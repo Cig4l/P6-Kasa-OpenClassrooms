d47f5bf0cedb897f7583ff67990e0be0b6d698e9
Cigale <coralieruiz.dev@gmail.com>
2024-04-19 12:16:33 +0200

    design description de produit mobile-first

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,10 +1,22 @@
 export default function Carousel (props) {
-    const prevArrow = "<";
-    const nextArrow = ">";
+    const prevArrow = <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 0 320 512"><path fill="#ffffff" d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg>;
+    const nextArrow = <svg xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 0 320 512"><path fill="#ffffff" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>;
+
+    const pictures = [
+        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg",
+        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-2.jpg",
+        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-3.jpg",
+        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-4.jpg",
+        "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-5.jpg"
+    ];
     
-    <div className="carousel-container" aria-label="carousel d'images">
-        <img className="carousel-image" src="#" />
-        <div className="nav-arrow" id="prev-arrow">{prevArrow}</div>
-        <div className="nav-arrow" id="next-arrow">{nextArrow}</div>
-    </div>
+    return (<>
+        <div className="carousel-container" aria-label="carousel d'images">
+            <img className="carousel-img" src={pictures[0]} />
+            <div className="nav-arrows-container">
+                <button className="nav-arrow" id="prev-arrow">{prevArrow}</button>
+                <button className="nav-arrow" id="next-arrow">{nextArrow}</button>
+            </div>
+        </div>
+    </>)
 }
