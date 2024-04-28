8bca6166396dc3ebd2cdcd9c93eca5b154c1a125
Cigale <coralieruiz.dev@gmail.com>
2024-04-28 21:44:17 +0200

    displayRating()

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,29 +3,20 @@ import React, { useEffect } from 'react';
 
 export default function NavBar(){
     const location = useLocation();
-
-    useEffect(() => {
-        var path = location.pathname;                // Obtenez l'URL de la page actuelle
-        if(path) {
-            console.log("path")
-        }
-        else {
-            console.log("not found");
-        }
-        }, []);
+    var currentPage = location.pathname;                // Obtenez l'URL de la page actuelle
 
     return (<>
         <header>
-            <Link to="/">
+            <Link to="/" className={currentPage === '/' ? 'active' : ''}>
                 <img src="src/assets/images/LOGO.svg" height="50" width="155.15" />
             </Link>
             <nav className="navbar">
                 <ul>
                     <li key="accueil">
-                        <Link to="/">Accueil</Link>
+                        <Link to="/" className={currentPage === '/' ? 'active' : ''}>Accueil</Link>
                     </li>
                     <li key="about">
-                        <Link to="/about">A propos</Link>
+                        <Link to="/about" className={currentPage === '/about' ? 'active' : ''}>A propos</Link>
                     </li>
                 </ul>
             </nav>
