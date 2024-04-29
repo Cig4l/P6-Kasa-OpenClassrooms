671ef21105cc3c647f9e29a7ae60f5442e14aaa7
Cigale <coralieruiz.dev@gmail.com>
2024-04-29 19:13:10 +0200

    intégration page erreur 404

--- src/Routes/PageNotFound.jsx
+++ src/Routes/PageNotFound.jsx
@@ -1,8 +1,13 @@
 import Layout from "../Layout.jsx"
+import { Link } from "react-router-dom";
 
 export default function PageNotFound(){
     const children = (<>
-        
+        <div className="not-found-container">
+            <img src="src/assets/images/404.svg" alt="Erreur 404" />
+            <p className="error-message">Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="redirection">Retourner sur la page d’accueil</Link>
+        </div>
     </>);
     
     return (<>
