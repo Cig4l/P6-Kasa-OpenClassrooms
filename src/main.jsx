111207dd190f34f0e3a5e522b7243b1b27cf59a9
Cigale <coralieruiz.dev@gmail.com>
2024-04-30 15:41:38 +0200

    Revert "vite.config.js"
    This reverts commit ea003998b8f2db79beb58f58b0adadd296aa959c.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './SCSS/index.scss';
+import { BrowserRouter, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/P6-Kasa-OpenClassrooms/', 
-  build: {
-    outDir: 'dist', 
-  },
   plugins: [react()],
 })
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
